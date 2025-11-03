4c6fc6b7ea8737d2d2e5adf376a8644cf5c272db
whoenykkk <whoneykim@gmail.com>
2025-11-03 23:51:42 +0900

    feat: 결과 출력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -24,6 +24,7 @@ public class Application {
         int bonusNumber = parseBonusNumber(Console.readLine(), winningNumbers);
 
         Map<Rank, Integer> results = checkWinnings(lottos, winningNumbers, bonusNumber);
+        printResults(results, purchaseAmount);
     }
 
     private static Map<Rank, Integer> checkWinnings(List<Lotto> lottos, List<Integer> winningNumbers, int bonusNumber) {
@@ -39,6 +40,27 @@ public class Application {
         }
 
         return results;
+    }
+
+    private static void printResults(Map<Rank, Integer> results, int purchaseAmount) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println("3개 일치 (5,000원) - " + results.get(Rank.FIFTH) + "개");
+        System.out.println("4개 일치 (50,000원) - " + results.get(Rank.FOURTH) + "개");
+        System.out.println("5개 일치 (1,500,000원) - " + results.get(Rank.THIRD) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + results.get(Rank.SECOND) + "개");
+        System.out.println("6개 일치 (2,000,000,000원) - " + results.get(Rank.FIRST) + "개");
+
+        double profitRate = calculateProfitRate(results, purchaseAmount);
+        System.out.println("총 수익률은 " + String.format("%.1f", profitRate) + "%입니다.");
+    }
+
+    private static double calculateProfitRate(Map<Rank, Integer> results, int purchaseAmount) {
+        int totalPrize = 0;
+        for (Rank rank : results.keySet()) {
+            totalPrize += rank.getPrize() * results.get(rank);
+        }
+        return (double) totalPrize / purchaseAmount * 100;
     }
 
     private static List<Lotto> generateLottos(int purchaseAmount) {
